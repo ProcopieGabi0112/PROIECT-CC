d9268737b757987709df7eedf8c01853e4ce9aaf
Procopie Gabriel <36031339+ProcopieGabi0112@users.noreply.github.com>
2023-01-12 19:29:15 +0200

    Delete proiect_cc directory

--- proiect_cc/hotel-service/src/main/java/com/project_cc/app_hotel/hotelservice/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project_cc.app_hotel.hotelservice.repository;
-
-import com.project_cc.app_hotel.hotelservice.model.Hotel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface HotelRepository extends MongoRepository<Hotel,String> {
-
-}
